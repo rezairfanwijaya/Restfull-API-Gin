18f33c65e0e1239bdde2d69e5abd616a2098b966
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-05-15 16:03:30 +0700

    add connection database to all router

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"log"
 	"restfull-api/handler"
+	"restfull-api/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	// import koneksi database
+	db := models.Connect()
+
 	// insiasi gin router
 	route := gin.Default()
+
+	// pasang koneksi database ke router
+	// code ini akan dijalankan pertama sebelum masuk ke handler
+	route.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next() // jika koneksi sudah berhasil maka lanjutkan ke handler
+	})
 
 	// definisi router dan route handler (di import dari handler/handler.go)
 	route.GET("/", handler.IndexHandler)
